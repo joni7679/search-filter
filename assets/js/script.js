7f4c8eb8c405839d11d66860269ecd630a7f9276
joni <codingmylife0@gmail.com>
2024-06-11 11:40:50 +0530

    crate a seacrh filter

---
+++ assets/js/script.js
@@ -0,0 +1,156 @@
+
+let data = [
+    {
+        hotelname: "raj hotel",
+        perday: 1000,
+        img: "https://images.unsplash.com/photo-1564501049412-61c2a3083791?q=80&w=1932&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        headline: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Deleniti natus accusantium nostrum tempora quas. Libero iure at distinctio praesentium. Maiores consequuntur dolorum voluptate.",
+        location: "mumbai",
+
+    },
+
+    {
+        hotelname: "todo hotel",
+        perday: 2000,
+        img: "https://images.unsplash.com/photo-1564501049412-61c2a3083791?q=80&w=1932&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        headline: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Deleniti natus accusantium nostrum tempora quas. Libero iure at distinctio praesentium. Maiores consequuntur dolorum voluptate.",
+        location: "mumbai",
+    },
+
+    {
+        hotelname: "todo hotel",
+        perday: 2000,
+        img: "https://plus.unsplash.com/premium_photo-1682091912967-7b5daf030418?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+
+        headline: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Deleniti natus accusantium nostrum tempora quas. Libero iure at distinctio praesentium. Maiores consequuntur dolorum voluptate.",
+        location: "ladak",
+    },
+
+    {
+        hotelname: "mumbai under ground zoo",
+        perday: 2000,
+        img: "https://plus.unsplash.com/premium_photo-1664298023756-3a4bdcb24036?q=80&w=2010&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+
+        headline: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Deleniti natus accusantium nostrum tempora quas. Libero iure at distinctio praesentium. Maiores consequuntur dolorum voluptate.",
+        location: "mumbai",
+    },
+
+    {
+        hotelname: "kolkata zoo ",
+        perday: 2000,
+        img: "https://images.unsplash.com/photo-1503919005314-30d93d07d823?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        headline: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Deleniti natus accusantium nostrum tempora quas. Libero iure at distinctio praesentium. Maiores consequuntur dolorum voluptate.",
+        location: "kolkata",
+    },
+
+    {
+        hotelname: "kolkata zoo ",
+        perday: 2000,
+        img: "https://plus.unsplash.com/premium_photo-1678286770016-6306ad61df9b?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        headline: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Deleniti natus accusantium nostrum tempora quas. Libero iure at distinctio praesentium. Maiores consequuntur dolorum voluptate.",
+
+        location: "kolkata",
+    },
+
+    {
+        hotelname: "delhi hotel",
+        perday: 2000,
+        img: "https://images.unsplash.com/photo-1611892440504-42a792e24d32?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+
+        headline: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Deleniti natus accusantium nostrum tempora quas. Libero iure at distinctio praesentium. Maiores consequuntur dolorum voluptate.",
+
+        location: "delhi",
+    },
+
+
+    {
+        hotelname: "delhi hotel",
+        perday: 2000,
+        img: "https://images.unsplash.com/photo-1522798514-97ceb8c4f1c8?q=80&w=1935&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+
+        headline: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Deleniti natus accusantium nostrum tempora quas. Libero iure at distinctio praesentium. Maiores consequuntur dolorum voluptate.",
+
+        location: "delhi",
+    },
+
+    {
+        hotelname: "delhi gate",
+        perday: 1500,
+        img: "https://imgs.search.brave.com/tZofgtnnlWzr6igvpN4Wy0xh6qj7ZaJotin40JTMqrc/rs:fit:860:0:0/g:ce/aHR0cHM6Ly90My5m/dGNkbi5uZXQvanBn/LzAwLzY1LzQxLzMw/LzM2MF9GXzY1NDEz/MDM3X01ueURYTFVI/Q1l6VEJrZjAwNmhX/TXdtMWxkbW44N3BM/LmpwZw",
+
+        headline: "Explore the main attractions of Calcutta on this all-day guided tour. First, visit Mother .",
+
+        location: "delhi",
+    },
+
+
+    {
+        hotelname: "delhi hotel",
+        perday: 2000,
+        img: "https://images.unsplash.com/photo-1522798514-97ceb8c4f1c8?q=80&w=1935&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        headline: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Deleniti natus accusantium nostrum tempora quas. Libero iure at distinctio praesentium. Maiores consequuntur dolorum voluptate.",
+
+        location: "LONDON",
+    },
+
+]
+
+let searchInput = document.querySelector("#searchInput");
+const searchOptionContainer = document.querySelector('.search-option-container'); // Assuming '.search-option-container' is the container where you want to display search options
+let searchOption = document.querySelector(".search-option")
+
+searchInput.addEventListener("focus", () => {
+    let overly = document.querySelector(".overly");
+    overly.style.display = "block"
+    searchOption.style.display = "block"
+})
+
+searchInput.addEventListener("blur", () => {
+    let overly = document.querySelector(".overly");
+    overly.style.display = "none"
+    searchOption.style.display = "none"
+})
+let productContainer = document.querySelector(".product-container");
+let sarchBox = document.querySelector(".navbar-center")
+// Select the search option container
+// let searchOptionContainer = document.querySelector(".searchOptionContainer");
+let search = document.querySelector(".search-option")
+
+searchInput.addEventListener("input", () => {
+    //
+
+    let clutter = "";
+    const filteredData = data.filter(item => item.location.toLowerCase().startsWith(searchInput.value.toLowerCase()));
+
+    // Clear previous search options
+    search.innerHTML = '';
+
+    filteredData.forEach(obj => {
+        clutter += `
+                <div class="search-list">
+                    <i class="ri-search-line"></i>
+                    <span>${obj.location}</span>
+                </div>`;
+    });
+    let iteam = "";
+    filteredData.forEach((element) => {
+        iteam += `<div class="product">
+            <div class="product-img">
+                <img src="${element.img}"
+                    alt="">
+            </div>
+            <div class="product-describction">
+                <h3>${element.hotelname}</h3>
+                <p>${element.headline}</P>
+                <p>  Mimmum  one person perday  Rs :-${element.perday}</p>
+             <p>location:-${element.location}</p>
+
+            </div>
+        </div>`
+    })
+
+    // Append generated search options to the container
+    search.innerHTML = clutter;
+    productContainer.innerHTML = iteam;
+});
+
